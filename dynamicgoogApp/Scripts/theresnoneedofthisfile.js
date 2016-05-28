99ef4a30c148bf2a5b07c3975c3d3720a0d363b6
Arun Balajiee <cs11b042@iith.ac.in>
2016-05-28 20:42:48 +0530

    First Commit: includes gives drive folder

---
+++ dynamicgoogApp/Scripts/theresnoneedofthisfile.js
@@ -0,0 +1,15 @@
+﻿var default_Text = "this works";
+
+var myapp = angular.module("dynaGoog", []);
+var func = myapp.controller('ngCtrl', ['$scope','$http',function ($scope,$http) {
+    $scope.name = default_Text;
+
+    $scope.url = 'https://script.google.com/a/macros/bwilliamsconsulting.com/s/AKfycbzhCB0-O8u-17z3y5hK6RXofBcuLHLITkOIsBxBVuzC2ihcpLO8/exec';
+    $scope.change = function (ev) {
+        if(ev == 13){
+        if ($scope.name != default_Text && $scope.name.length>10) {
+            callScriptFunction();
+        }
+        }
+    }
+}]);
